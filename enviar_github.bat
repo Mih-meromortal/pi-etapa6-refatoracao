c1676833083e040e5219a4781e779ddfdf4a2d65
Mih Almeida <almeida.michelemarques@gmail.com>
2026-09-09 13:33:55 -0300

    feat: inclusao dos testes unitarios JUnit da Etapa 7

--- enviar_github.bat
+++ enviar_github.bat
@@ -1,15 +1,8 @@
 @echo off
-echo target/ > .gitignore
-echo .idea/ >> .gitignore
-echo *.class >> .gitignore
-
-git init
-git add .
-git commit -m "feat: refatoracao de arquitetura desktop para desacoplamento web"
-git branch -M main
-git remote add origin https://github.com/Mih-meromortal/pi-etapa6-refatoracao.git
-git push -u origin main
+git add -A
+git commit -m "feat: inclusao dos testes unitarios JUnit da Etapa 7"
+git push origin main
 
 echo.
-echo Processo concluido com sucesso!
+echo Testes enviados ao GitHub!
 pause
